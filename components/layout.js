7939c47738c8e6874274ec2d63f54e39669c89ca
rampoi <iga6poi@outlook.jp>
2022-02-27 10:47:29 +0900

    imgix追加

--- components/layout.js
+++ components/layout.js
@@ -34,6 +34,7 @@ export default function Layout({ children, home }) {
               className={utilStyles.borderCircle}
               height={144}
               width={144}
+              quality={50}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
