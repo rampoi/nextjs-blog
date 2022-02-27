9be30ade7995cae758da6ac9191b913f6c50b0d4
rampoi <iga6poi@outlook.jp>
2022-02-28 08:13:02 +0900

    img変更

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     },
 }
 
-module.exports = {
-    basePath: process.env.GITHUB_ACTIONS ? "/nextjs-blog" : "",
-    trailingSlash: true,
-  };
+// module.exports = {
+//     basePath: process.env.GITHUB_ACTIONS ? "/nextjs-blog" : "",
+//     trailingSlash: true,
+//   };
